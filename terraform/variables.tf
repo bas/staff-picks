eea9ebd4ef21b10e414ce9298aa4ae85031d52f4
Bas Peters <bas@users.noreply.github.com>
2022-11-01 10:10:55 +0100

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "LAUNCHDARKLY_ACCESS_TOKEN" {
+  type = string
+}
+
+variable "project" {
+  default = "bas-staff-picks-demo"
+}
+
+variable "bucket" {
+  default = "bas-staff-picks-demo"
+}
+
+variable "region" {
+  default = "eu-west-1"
+}

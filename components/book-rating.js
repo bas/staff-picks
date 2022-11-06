87f60079fd8d855d0de04fc56870c2df826855d6
Bas Peters <bas@github.com>
2022-11-06 12:19:43 +0100

    Refactor book rating

---
+++ components/book-rating.js
@@ -0,0 +1,22 @@
+import { Box, Text } from "@primer/react";
+import { StarFillIcon } from "@primer/octicons-react";
+
+function BookRating({ stars }) {
+  return (
+    <Box pt={6}>
+      <Text as="span" sx={{ fontSize: 1 }}>
+        Rating:
+      </Text>
+      <Text as="span" sx={{ fontSize: 1, marginLeft: ".5rem" }}>
+        {[...Array(stars)].map((e, i) => (
+          <StarFillIcon key={i} size={16} fill="#FFD700" />
+        ))}
+        {[...Array(5 - stars)].map((e, i) => (
+          <StarFillIcon key={i} size={16} fill="#E5E4E2" />
+        ))}
+      </Text>
+    </Box>
+  );
+}
+
+export default BookRating;

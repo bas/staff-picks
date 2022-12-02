c08825ffc472907da5740446b4ec7a277f73644a
Bas Peters <bas@github.com>
2022-12-02 13:26:12 +0100

    Viva Magenta

--- components/banner.js
+++ components/banner.js
@@ -2,7 +2,7 @@ import { Box, Text } from "@primer/react";
 
 function Banner({ couponCode, discount }) {
   return (
-    <Box sx={{ backgroundColor: "#FF1493", borderRadius: 5, width: "fit-content", margin: "1em auto" }} p={3}>
+    <Box sx={{ backgroundColor: "#BE3455 ", borderRadius: 5, width: "fit-content", margin: "1em auto" }} p={3}>
       <Text sx={{ fontSize: 3, color: "white", fomtWeight: "bold" }}>
         Apply coupon code {couponCode} to get {discount}% discount on checkout!
       </Text>

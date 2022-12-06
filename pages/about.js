da1d144a522ceb813b6e3b56a678c76e66b50aaa
Bas Peters <bas@github.com>
2022-12-06 09:03:23 +0100

    Add content box

--- pages/about.js
+++ pages/about.js
@@ -21,121 +21,122 @@ export default function About() {
             </Heading>
           </PageLayout.Header>
           <PageLayout.Content>
-            <Text as="p" padding="0px 12px 0px 12px">
-              The following identities are avaiable for targeting:
-            </Text>
-            <Box
-              padding="12px"
-              display="grid"
-              gridTemplateColumns="1fr 1fr 1fr 1fr 1fr 1fr"
-              gridGap={1}
-            >
+            <Box p={12}>
+              <Text as="p">
+                The following identities are avaiable for targeting:
+              </Text>
               <Box
-                p={3}
-                borderColor="border.default"
-                borderWidth={1}
-                borderStyle="solid"
-                fontWeight="bold"
+                display="grid"
+                gridTemplateColumns="1fr 1fr 1fr 1fr 1fr 1fr"
+                gridGap={1}
               >
-                Key
+                <Box
+                  p={3}
+                  borderColor="border.default"
+                  borderWidth={1}
+                  borderStyle="solid"
+                  fontWeight="bold"
+                >
+                  Key
+                </Box>
+                <Box
+                  p={3}
+                  borderColor="border.default"
+                  borderWidth={1}
+                  borderStyle="solid"
+                  fontWeight="bold"
+                >
+                  Name
+                </Box>
+                <Box
+                  p={3}
+                  borderColor="border.default"
+                  borderWidth={1}
+                  borderStyle="solid"
+                  fontWeight="bold"
+                >
+                  Region
+                </Box>
+                <Box
+                  p={3}
+                  borderColor="border.default"
+                  borderWidth={1}
+                  borderStyle="solid"
+                  fontWeight="bold"
+                >
+                  Country
+                </Box>
+                <Box
+                  p={3}
+                  borderColor="border.default"
+                  borderWidth={1}
+                  borderStyle="solid"
+                  fontWeight="bold"
+                >
+                  City (private)
+                </Box>
+                <Box
+                  p={3}
+                  borderColor="border.default"
+                  borderWidth={1}
+                  borderStyle="solid"
+                  fontWeight="bold"
+                >
+                  Premium
+                </Box>
+                {identities.map((identity) => (
+                  <React.Fragment key={identity.key}>
+                    <Box
+                      p={3}
+                      borderColor="border.default"
+                      borderWidth={1}
+                      borderStyle="solid"
+                    >
+                      {identity.key}
+                    </Box>
+                    <Box
+                      p={3}
+                      borderColor="border.default"
+                      borderWidth={1}
+                      borderStyle="solid"
+                    >
+                      {identity.name}
+                    </Box>
+                    <Box
+                      p={3}
+                      borderColor="border.default"
+                      borderWidth={1}
+                      borderStyle="solid"
+                    >
+                      {identity.custom.region}
+                    </Box>
+                    <Box
+                      p={3}
+                      borderColor="border.default"
+                      borderWidth={1}
+                      borderStyle="solid"
+                    >
+                      {identity.country}
+                    </Box>
+                    <Box
+                      p={3}
+                      borderColor="border.default"
+                      borderWidth={1}
+                      borderStyle="solid"
+                    >
+                      {identity.custom.city}
+                    </Box>
+                    <Box
+                      p={3}
+                      borderColor="border.default"
+                      borderWidth={1}
+                      borderStyle="solid"
+                    >
+                      {identity.custom.premium ? "Yes" : "No"}
+                    </Box>
+                  </React.Fragment>
+                ))}
               </Box>
-              <Box
-                p={3}
-                borderColor="border.default"
-                borderWidth={1}
-                borderStyle="solid"
-                fontWeight="bold"
-              >
-                Name
-              </Box>
-              <Box
-                p={3}
-                borderColor="border.default"
-                borderWidth={1}
-                borderStyle="solid"
-                fontWeight="bold"
-              >
-                Region
-              </Box>
-              <Box
-                p={3}
-                borderColor="border.default"
-                borderWidth={1}
-                borderStyle="solid"
-                fontWeight="bold"
-              >
-                Country
-              </Box>
-              <Box
-                p={3}
-                borderColor="border.default"
-                borderWidth={1}
-                borderStyle="solid"
-                fontWeight="bold"
-              >
-                City (private)
-              </Box>
-              <Box
-                p={3}
-                borderColor="border.default"
-                borderWidth={1}
-                borderStyle="solid"
-                fontWeight="bold"
-              >
-                Premium
-              </Box>
-              {identities.map((identity) => (
-                <React.Fragment key={identity.key}>
-                  <Box
-                    p={3}
-                    borderColor="border.default"
-                    borderWidth={1}
-                    borderStyle="solid"
-                  >
-                    {identity.key}
-                  </Box>
-                  <Box
-                    p={3}
-                    borderColor="border.default"
-                    borderWidth={1}
-                    borderStyle="solid"
-                  >
-                    {identity.name}
-                  </Box>
-                  <Box
-                    p={3}
-                    borderColor="border.default"
-                    borderWidth={1}
-                    borderStyle="solid"
-                  >
-                    {identity.custom.region}
-                  </Box>
-                  <Box
-                    p={3}
-                    borderColor="border.default"
-                    borderWidth={1}
-                    borderStyle="solid"
-                  >
-                    {identity.country}
-                  </Box>
-                  <Box
-                    p={3}
-                    borderColor="border.default"
-                    borderWidth={1}
-                    borderStyle="solid"
-                  >
-                    {identity.custom.city}
-                  </Box>
-                  <Box
-                    p={3}
-                    borderColor="border.default"
-                    borderWidth={1}
-                    borderStyle="solid"
-                  >
-                    {identity.custom.premium ? "Yes" : "No"}
-                  </Box>
-                </React.Fragment>
-              ))}
             </Box>
           </PageLayout.Content>
           <PageLayout.Footer>

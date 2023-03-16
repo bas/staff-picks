08ee8a31bf3f31ff1cd6702108f59a0a900cd9da
Bas Peters <bas@github.com>
2023-03-16 18:03:55 +0100

    Add link element

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -3,6 +3,7 @@ import { useFlags } from "launchdarkly-react-client-sdk";
 import Image from "next/image";
 import logoPic from "../public/images/logo.png";
 import LoginForm from "./login-form";
+import Link from "next/link";
 
 function NavigationBar() {
   const { showLogin } = useFlags();
@@ -18,10 +19,10 @@ function NavigationBar() {
         <Image src={logoPic} alt="logo" height="20" width="20" />
       </Header.Item>
       <Header.Item>
-        <Header.Link href={`${process.env.assetPrefix}/`}>Home</Header.Link>
+        <Header.Link as={Link} href={`${process.env.assetPrefix}/`}>Home</Header.Link>
       </Header.Item>
       <Header.Item full>
-        <Header.Link href={`${process.env.assetPrefix}/about`}>
+        <Header.Link as={Link} href={`${process.env.assetPrefix}/about`}>
           About
         </Header.Link>
       </Header.Item>

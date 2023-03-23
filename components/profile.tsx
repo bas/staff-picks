2b42bfd8860fd266bd5666532630904fb9c926fc
bas <bpeters@launchdarkly.com>
2023-03-23 12:09:00 +0100

    Create profile component

---
+++ components/profile.tsx
@@ -0,0 +1,44 @@
+import {
+  Box,
+  TextInput,
+  Button,
+  Popover,
+  Text,
+  IconButton,
+  Heading,
+} from "@primer/react";
+import { MailIcon } from "@primer/octicons-react";
+
+function Profile({ user, isOpen, setOpen }) {
+
+  return (
+    <Box justifyContent="right" display="flex" color="black">
+      <Popover open={isOpen} caret="top-right">
+        <Popover.Content>
+          <Heading sx={{ fontSize: 2 }}>Profile</Heading>
+          <Text as="p" sx={{ whiteSpace: "nowrap" }}>
+            <MailIcon size={16} /> {user.email}
+          </Text>
+          <Text as="p">Name: {user.name}</Text>
+          <Text as="p">Country: {user.country}</Text>
+          <Text as="p">Premium: {user.isPremium ? "Yes" : "No"}</Text>
+          <Text as="p">Staff: {user.isStaff ? "Yes" : "No"}</Text>
+          <Text as="p">Beta: {user.isBeta ? "Yes" : "No"}</Text>
+          <Text as="p">Categories: {user.categories.join(", ")}</Text>
+          <Text as="p">Device: {user.device}</Text>
+          <Text as="p">OS: {user.operatingSystem}</Text>
+          <Box display="flex">
+            <Button
+              variant="primary"
+              onClick={() => (isOpen ? setOpen(false) : setOpen(true))}
+            >
+              Close
+            </Button>
+          </Box>
+        </Popover.Content>
+      </Popover>
+    </Box>
+  );
+}
+
+export default Profile;

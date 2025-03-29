4010f04cfd159eec57f61f6da582e7c9dfa39e76
Bas Peters <bas@github.com>
2025-03-29 14:26:24 +0100

    Remove GitHub Actions configuration from Next.js setup

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*\//, "");
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
-module.exports = {
-  assetPrefix,
-  basePath,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     unoptimized: true, // Disable server-side image optimization for static export
   },
   trailingSlash: true, // Ensure trailing slashes for static export
 };
+
+module.exports = nextConfig;
